3b5e1a581eab57bfb410092c74879b7b6620f375
jpxiong <xiongjipu@gmail.com>
2015-08-07 16:06:30 +0800

    release v1.2.3

--- src/common/MessageConfig.java
+++ src/common/MessageConfig.java
@@ -7,4 +7,6 @@ public class MessageConfig {
     public static final String ILLEGAL_TIME_MSG = "Illegal startTime or endTime!";
     public static final String ILLEGAL_TITLE_MSG = "The length of title should be at least:" + Config.TITLE_MIN_LENGTH 
             + ",or at most:" + Config.TITLE_MAX_LENGTH;
+    public static final String ILLEGAL_FILE_NAME_EXCEPTION_MSG = "Illegal file name !";
+    public static final String ILLEGAL_FORMAT_EXCEPTION_MSG = "Illegal format !";
 }
